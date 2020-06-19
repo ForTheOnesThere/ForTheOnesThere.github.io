1848ec25c9b8c7431da229c489d1b110060f76e3
James Cockayne <jamescockayne1@gmail.com>
2020-06-19 21:30:18 +0100

    Added animations for new component

--- src/components/GithubCard/GithubCard.js
+++ src/components/GithubCard/GithubCard.js
@@ -19,62 +19,49 @@ const GithubCard = ({ data }) => {
           <section className='gh' id='github'>
               <div id='git-card-wrapper'>
 
+                <Fade bottom duration={1000} delay={250} distance="50px">
+
                   <Title title="Online Presence" />
 
-                  <img src={require('../../images/logo.png')} alt='Github Logo' className='img'/>
+                  <Tilt
+                    options={{
+                      reverse: false,
+                      max: 8,
+                      perspective: 1000,
+                      scale: 1,
+                      speed: 300,
+                      transition: true,
+                      axis: null,
+                      reset: true,
+                      easing: 'cubic-bezier(.03,.98,.52,.99)',
+                    }}
+                  >
+                    <img src={require('../../images/logo.png')} alt='Github Logo' className='img'/>
+                  </Tilt>
 
-                  <br/>
-                  <br/>
+                </Fade>
 
+                <br/>
+                <br/>
+
+                <Fade bottom duration={1000} delay={250} distance="50px">
                   <p className='with-edge-space'>{text}</p>
 
                   <br/>
                   <br/>
 
-                <ScrollIntoView selector="#footer">
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cta-btn cta-btn--hero"
-                    >
-                    Take A Look
-                  </a>
-                </ScrollIntoView>
-
+                  <ScrollIntoView selector="#footer">
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--hero"
+                      >
+                      Take A Look
+                    </a>
+                  </ScrollIntoView>
+                </Fade>
               </div>
             </section>
-
-
-
-
-  /*
-      <Fade bottom>
-
-      <div className='gh-container'>
-<div id='gh-su'>
-
-
-
-
-
-
-            <h1 className='pa1 title'>Online Presence</h1>
-
-
-
-
-
-
-          </div>
-
-
-
-      </div>
-
-      </Fade>
-
-    */
-
   );
 }
 
